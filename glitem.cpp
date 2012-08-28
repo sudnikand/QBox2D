63103bad6f24099c0abf4b9b1c96c5b0200932ac
Andrey <sudnikand@gmail.com>
2012-08-29 01:04:21 +0700

    Initail complete of GLItem class

--- glitem.cpp
+++ glitem.cpp
@@ -1,6 +1,5 @@
 #include "glitem.h"
-#include <QtOpenGL>
-
+#include "def.h"
 GLItem::GLItem()
 {
 }
@@ -12,19 +11,36 @@ void GLItem::createBody(b2World* const world){
 
 void GLItem::render(){
     if(!_physic.body()) return;
+    
+    const b2Shape *shape = body()->GetFixtureList()->GetShape();
+    if (!shape) return;
     glPushMatrix();
 
     const b2Vec2 &position = body()->GetPosition();
-    glTranslatef(position.x,position.y,0.0f);
-    glRotatef(body()->GetAngle(),0.0f,0.0f,1.0f);
+    glTranslatef(position.x, position.y, 0.0f);
+    const float32 &angle = body()->GetAngle();
+    glRotatef(RAD2ANG(angle),0,0,1.0f);
+    glBegin(GL_TRIANGLE_FAN);
+    
+    if( shape->GetType() == b2Shape::e_polygon ) {
+        glColor3f(_color.redF(), _color.greenF(), _color.blueF());
+        for (int32 i=0; i < ((b2PolygonShape*)shape)->GetVertexCount(); ++i){
+            const b2Vec2 vertex = ((b2PolygonShape*)shape)->GetVertex(i);
+            glVertex2f(vertex.x, vertex.y);
+        }
+    } else if (shape->GetType() == b2Shape::e_circle ) {
+        glColor3f(_color.redF(), _color.greenF(), _color.blueF());
+        glVertex2f(0,0);
+        int accuracy = 32;
+        float32 radius = shape->m_radius;
+        float32 a = 2 * PI/accuracy;
+        for(int i = 0; i < accuracy; ++i) {
+            glVertex2f( cos(a*i) * radius, sin(a*i) * radius);
+        }
+        glVertex2f(radius, 0);
+    }
 
-    glBegin(GL_TRIANGLES);
-    glColor3f(1,0,0);
-    glVertex3f(0,0,0);
-    glColor3f(0,1,0);
-    glVertex3f(50,0,0);
-    glColor3f(0,0,1);
-    glVertex3f(50,50,0);
+
     glEnd();
 
     glPopMatrix();
--- glitem.h
+++ glitem.h
@@ -2,6 +2,7 @@
 #define GLITEM_H
 
 #include "physicitem.h"
+#include <QtOpenGL>
 
 class GLItem
 {
@@ -9,8 +10,8 @@ public:
     GLItem();
     void render();
     void createBody    (b2World* const world);
-    void createFixture ();
-    void setPos        (const float32 &x, const float32 &y){ _physic.setPos(b2Vec2(x,y)); }
+    void setBrush      (const QColor  & c  )     { _color = c; }
+    void setPos        (const float32 &x, const float32 &y) { _physic.setPos(b2Vec2(x,y)); }
     void setPos        (const b2Vec2  & pos)     { _physic.setPos(pos); }
     void setRotation   (const float32 & rot)     { _physic.setRotation(rot); }
     void setShape      (const b2Shape & s  )     { _physic.setShape(s); }
@@ -18,10 +19,13 @@ public:
     void setDensity    (const float32 & d  )     { _physic.setDensity(d); }
     void setFriction   (const float32 & f  )     { _physic.setFriction(f); }
     void setBodyType   (const b2BodyType & bt)   { _physic.setBodyType(bt); }
+    void createFixture ()                        { _physic.createFixture(); }
+
     b2Body* body() const { return _physic.body(); }
 
 private:
     PhysicItem _physic;
+    QColor     _color;
 };
 
 #endif // GLITEM_H
