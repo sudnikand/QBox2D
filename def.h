18fbf03b94b0e56c76acf1712becc1e94df8d6ac
Andrey <sudnikand@gmail.com>
2012-08-29 01:01:33 +0700

    move QDebug in def.h

--- def.h
+++ def.h
@@ -1,6 +1,8 @@
 #ifndef DEF_H
 #define DEF_H
 
+#include <QDebug>
+
 #define PI 3.141592653589793238463
 #define ANG2RAD( a ) ( ( (a) * PI ) / 180.0f )
 #define RAD2ANG( a ) ( ( (a) * 180.0f ) / PI )
