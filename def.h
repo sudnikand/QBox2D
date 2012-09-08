d5e79256f98cb4285fb763ee75788ce6f8d8be3b
Andrey <sudnikand@gmail.com>
2012-09-09 03:24:22 +0700

    Fix scale factor - float

--- def.h
+++ def.h
@@ -7,7 +7,7 @@
 #define ANG2RAD( a ) ( ( (a) * PI ) / 180.0f )
 #define RAD2ANG( a ) ( ( (a) * 180.0f ) / PI )
 
-#define WORLD_SCALE_VALUE 50
+#define WORLD_SCALE_VALUE 50.0f
 
 #define W2Q(x,y)    ( x * WORLD_SCALE_VALUE ),( y * WORLD_SCALE_VALUE )
 #define W2Q_(x)     ( x * WORLD_SCALE_VALUE )
