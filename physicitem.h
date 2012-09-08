0833b31d69856958ce72dc9bffe960b5bfda5891
Andrey <sudnikand@gmail.com>
2012-09-09 03:23:04 +0700

    Fix setting rotation

--- physicitem.h
+++ physicitem.h
@@ -12,7 +12,7 @@ public:
             void setDensity    (const float32 & d)     { _fd.density = d; }
             void setFriction   (const float32 & f)     { _fd.friction = f; }
             void setBodyType   (const b2BodyType & bt) { _bd.type = bt; }
-            void setRotation   (const float32 & r)     { _bd.angle = r; }
+            void setRotation   (const float32 & r)     { _bd.angle = ANG2RAD(r); }
             void setPos        (const b2Vec2 & p)      { _bd.position = p; }
             void setShape      (const b2Shape &s );
             void setUserData   (void *data);
