412a062291a730b257f1e5f81807edfd22b92705
Andrey <sudnikand@gmail.com>
2012-09-13 18:03:10 +0700

    Scale OpenGL rendering

--- items.cpp
+++ items.cpp
@@ -8,7 +8,7 @@ void QBox2DItem::render(){
     if (!shape) return;
     glPushMatrix();
 
-    glTranslatef(position().x, position().y, 0.0f);
+    glTranslatef(W2Q_(position().x), W2Q_(position().y), 0.0f);
     glRotatef(RAD2ANG(rotation()),0,0,1.0f);
     glBegin(GL_TRIANGLE_FAN);
 
@@ -16,13 +16,13 @@ void QBox2DItem::render(){
         glColor3f(_color.redF(), _color.greenF(), _color.blueF());
         for (int32 i=0; i < ((b2PolygonShape*)shape)->GetVertexCount(); ++i){
             const b2Vec2 vertex = ((b2PolygonShape*)shape)->GetVertex(i);
-            glVertex2f(vertex.x, vertex.y);
+            glVertex2f(W2Q(vertex.x, vertex.y));
         }
     } else if (shape->GetType() == b2Shape::e_circle ) {
         glColor3f(_color.redF(), _color.greenF(), _color.blueF());
         glVertex2f(0,0);
         int accuracy = 32;
-        float32 radius = shape->m_radius;
+        float32 radius = W2Q_(shape->m_radius);
         float32 a = 2 * PI/accuracy;
         for(int i = 0; i < accuracy; ++i) {
             glVertex2f( cos(a*i) * radius, sin(a*i) * radius);
