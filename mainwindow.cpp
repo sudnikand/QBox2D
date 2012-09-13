3f08dd8ff3a51a49f4a039c632aaeac19719d3eb
Andrey <sudnikand@gmail.com>
2012-09-13 22:51:25 +0700

    Switch to GLScene

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,43 +8,58 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    scene = new QScene(this);
-    scene->setItemIndexMethod(QGraphicsScene::NoIndex);
-    scene->setBackgroundBrush(Qt::white);
-    scene->setStickyFocus(false);
-
+    //scene = new QScene(this);
+    //scene->setItemIndexMethod(QGraphicsScene::NoIndex);
+    //scene->setBackgroundBrush(Qt::white);
+    //scene->setStickyFocus(false);
+    qDebug()<<"Creating GL Scene";
     glscene = new GLScene(this);
 
-    view = new QBox2DView(ui->centralWidget);
-    view->setScene(scene);
-    connect(ui->actionZoomIn, SIGNAL(triggered()), view, SLOT(zoomIn()));
-    connect(ui->actionZoomOut, SIGNAL(triggered()), view, SLOT(zoomOut()));
-    ui->horizontalLayout->addWidget(view);
+    //view = new QBox2DView(ui->centralWidget);
+    //view->setScene(scene);
+    //connect(ui->actionZoomIn, SIGNAL(triggered()), view, SLOT(zoomIn()));
+    //connect(ui->actionZoomOut, SIGNAL(triggered()), view, SLOT(zoomOut()));
+    connect(ui->actionZoomIn, SIGNAL(triggered()), glscene, SLOT(zoomIn()));
+    connect(ui->actionZoomOut, SIGNAL(triggered()), glscene, SLOT(zoomOut()));
+    //ui->horizontalLayout->addWidget(view);
     ui->horizontalLayout->addWidget(glscene);
-    view->fitInView(QRectF(0,0,100,100),Qt::KeepAspectRatioByExpanding);
+    //view->fitInView(QRectF(0,0,100,100),Qt::KeepAspectRatioByExpanding);
 
     createWorld();
 
     timer = new QTimer(this);
+    //connect(timer,SIGNAL(timeout()),this,SLOT(update()));
+    connect(timer,SIGNAL(timeout()),world,SLOT(step()),Qt::QueuedConnection);
+    connect(timer,SIGNAL(timeout()),glscene,SLOT(updateGL()),Qt::QueuedConnection);
+    /*
     connect(view,SIGNAL(mouseLeftButtonPressed(QPointF)),world,SLOT(grabItem(QPointF)));
     connect(view,SIGNAL(mouseRightButtonPressed(QPointF)),world,SLOT(createBox(QPointF)));
     connect(view,SIGNAL(mouseLeftButtonReleased()),world,SLOT(dropItem()));
     connect(view,SIGNAL(mouseMoved(QPointF)),world,SLOT(moveItem(QPointF)));
     connect(view,SIGNAL(keyPressed(int)),world,SLOT(handleKeyPressed(int)));
     connect(view,SIGNAL(keyReleased(int)),world,SLOT(handleKeyReleased(int)));
-    connect(timer,SIGNAL(timeout()),this,SLOT(update()));
+    */
+    connect(glscene,SIGNAL(mouseLeftButtonPressed(QPointF)),world,SLOT(grabItem(QPointF)));
+    connect(glscene,SIGNAL(mouseRightButtonPressed(QPointF)),world,SLOT(createBox(QPointF)));
+    connect(glscene,SIGNAL(mouseLeftButtonReleased()),world,SLOT(dropItem()));
+    connect(glscene,SIGNAL(mouseMoved(QPointF)),world,SLOT(moveItem(QPointF)));
+    connect(glscene,SIGNAL(keyPressed(int)),world,SLOT(handleKeyPressed(int)));
+    connect(glscene,SIGNAL(keyReleased(int)),world,SLOT(handleKeyReleased(int)));
+
 
     timer->start(1000/60);
 
 }
 
 void MainWindow::createWorld(){
+    qDebug()<<"Creating World";
     //world = new TestWorld();
     //world = new ExampleWorld();
     world = new ArcanoidWorld();
-
+/*
     connect(world,SIGNAL(itemCreated(QBox2DItem*)),scene,SLOT(addItem(QBox2DItem*)));
     connect(world,SIGNAL(itemDestroyed(QBox2DItem*)),scene,SLOT(removeItem(QBox2DItem*)));
+    */
     connect(world,SIGNAL(itemCreated(QBox2DItem*)),glscene,SLOT(addItem(QBox2DItem*)));
     connect(world,SIGNAL(itemDestroyed(QBox2DItem*)),glscene,SLOT(removeItem(QBox2DItem*)));
 
@@ -53,15 +68,15 @@ void MainWindow::createWorld(){
 }
 
 void MainWindow::update(){
-    world->step();
-    scene->advance();
+    //world->step();
+    //scene->advance();
     glscene->updateGL();
 }
 
 MainWindow::~MainWindow(){
     delete timer;
     delete world;
-    delete view;
-    delete scene;
+    //delete view;
+    //delete scene;
     delete ui;
 }
