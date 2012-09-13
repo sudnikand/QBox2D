96516e36e60bc855beee9b3692cfeb57245b5e04
Andrey <sudnikand@gmail.com>
2012-09-13 22:50:39 +0700

    Fix TestWorld Crashing

--- worlds/testworld.cpp
+++ worlds/testworld.cpp
@@ -4,18 +4,18 @@ TestWorld::TestWorld() : QBox2DWorld() {
 }
 
 void TestWorld::create(){
+    qDebug()<<"Creating TestWorld";
     _world->SetGravity(b2Vec2(0, 10));
     {
     QBox2DItem *ground = new QBox2DItem();
     ground->setPos(b2Vec2(0, 0));
     ground->createBody(_world);
+    ground->body()->SetUserData(ground);
     b2PolygonShape shape;
     shape.SetAsBox(20,1);
     ground->setShape(shape);
+    ground->setColor(QColor(64,80,64));
     appendItem(ground);
-    ground->graphics()->setBrush(QColor(64,80,64));
-    ground->body()->SetUserData(ground);
-
     }
 
     {
@@ -27,13 +27,13 @@ void TestWorld::create(){
     ball->setDensity(1.0f);
     ball->setRestitution(0.5f);
     ball->createBody(_world);
+    ball->body()->SetUserData(ball);
 
     b2CircleShape circle;
     circle.m_radius = radius;
     ball->setShape(circle);
+    ball->setColor(QColor(128 + qrand() % 128, 128 + qrand() % 128, 128 + qrand() % 128));
     appendItem(ball);
-    ball->graphics()->setBrush(QColor(128 + qrand() % 128, 128 + qrand() % 128, 128 + qrand() % 128));
-    ball->body()->SetUserData(ball);
     }
 }
 
