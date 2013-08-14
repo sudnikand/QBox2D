e42c2c88f683cd647f5ae6d069b37786e6e9a3af
Andrey Sudnik <sudnikand@yandex.ru>
2013-08-15 01:17:35 +0700

    Texturing exampleworld

--- worlds/exampleworld.cpp
+++ worlds/exampleworld.cpp
@@ -23,12 +23,13 @@ void ExampleWorld::populate(){
         rect.SetAsBox(size,size);
         box->setShape(rect);
         box->setColor(QColor(128 + qrand() % 128, 128 + qrand() % 128, 128 + qrand() % 128));
+        box->_textureName = "exit.png";
         appendItem(box);
     }
 
     // add triangles
     for (int i = 0; i < 10; ++i) {
-        float32 size = ((qrand() % 40) + 10.0f)/50;
+        float32 size = ((qrand() % 40) + 10.0f)/40;
         QBox2DItem *testItem = new QBox2DItem();
 
         testItem->setPos(b2Vec2(qrand() % 20 - 10.0f, -15.0f));
@@ -43,12 +44,13 @@ void ExampleWorld::populate(){
         shape.Set(&vertex.toStdVector().at(0),vertex.size());
         testItem->setShape(shape);
         testItem->setColor(QColor(128 + qrand() % 128, 128 + qrand() % 128, 128 + qrand() % 128));
+        testItem->_textureName = "kde.png";
         appendItem(testItem);
     }
 
 
 
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 50; ++i) {
         QBox2DItem *ball = new QBox2DItem();
         ball->setBodyType(b2_dynamicBody);
         ball->setPos(b2Vec2((qrand() % 300)/10.0f - 15.0f, (qrand() % 100)/10.0f - 10.0f));
@@ -57,12 +59,17 @@ void ExampleWorld::populate(){
         ball->setRestitution(0.5f);
         ball->createBody(_world);
 
-        float32 radius = 0.3f;
+        float32 radius = 1.1f;
         b2CircleShape circle;
         circle.m_radius = radius;
         ball->setShape(circle);
         ball->setColor(QColor(128 + qrand() % 128, 128 + qrand() % 128, 128 + qrand() % 128));
         ball->body()->SetUserData(ball);
+        if (qrand() % 2) {
+            ball->_textureName = "face-smile.png";
+        } else {
+            ball->_textureName = "face-surprise.png";
+        }
         appendItem(ball);
     }
 }
