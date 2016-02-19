9034d8fa337a056c8b3b405d7250cc0ac0426903
Andrey Sudnik <sudnikand@yandex.ru>
2016-02-20 03:44:31 +0600

    Added paths for resources data

--- def.h
+++ def.h
@@ -1,13 +1,19 @@
-#ifndef DEF_H
-#define DEF_H
+#ifndef __DEF_H__
+#define __DEF_H__
 
 #include <QDebug>
+
+#define DATA_DIR "../data/"
+#define SHADER_DIR   ( DATA_DIR "shaders/"  )
+#define TEXTURE_DIR  ( DATA_DIR "textures/" )
+#define LEVELS_DIR   ( DATA_DIR "levels/"   )
+#define RESOURCE_DIR ( DATA_DIR "res/"      )
 
 #define PI 3.141592653589793238463
 #define ANG2RAD( a ) ( ( (a) * PI ) / 180.0f )
 #define RAD2ANG( a ) ( ( (a) * 180.0f ) / PI )
 
-#define WORLD_SCALE_FACTOR 1
+#define WORLD_SCALE_FACTOR 1.0f
 
 #define WSCALE(x) (x) * WORLD_SCALE_FACTOR
 
